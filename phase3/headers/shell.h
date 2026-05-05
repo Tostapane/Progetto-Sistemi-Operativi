d08aca8b2fed0e124936c934d3b984e082692e5d
an3 <168350124+an3-dd@users.noreply.github.com>
2026-05-05 15:48:45 +0200

    shell.c e shell.h completi ma non testati

---
+++ phase3/headers/shell.h
@@ -0,0 +1,34 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <uriscv/liburiscv.h>
+
+#define NUM_PROGRAMS 3
+
+/* Struttura che definisce un comando, composto da
+ * il suo nome e il suo ASID (Address Space Identifier) */
+typedef struct command {
+  char *name;
+  int asid;
+} command;
+
+extern command programs[];
+
+// Ritorna la lunghezza di arg
+int myStrlen(char *arg);
+
+// Ritorna 0 se arg1 == arg2
+int myStrcmp(char *s1, char *s2);
+
+/* La shell stampa la riga "PandOSsh>> ", poi esegue la funzione SYSCALL
+ * READTERMINAL, che permette all'utente di scrivere e blocca la shell finche
+ * quest ultimo non preme invio.
+ *
+ * Viene analizzato il comando inserito dall'utente:
+ * - la shell termina se il comando inserito è 'exit'.
+ * - altrimenti effettua una ricerca del comando digitato.
+ *   - se il comando viene trovato viene eseguito
+ *   - altrimenti viene stampato un messaggio d'errore.
+ * */
+
+#endif
